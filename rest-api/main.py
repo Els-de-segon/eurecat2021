6f0924f856894a9afe85e0af0a5b348f364a46a8
Marc Gaspà <mgaspjoval2002@gmail.com>
2021-11-27 13:16:56 +0100

    flask example

--- rest-api/main.py
+++ rest-api/main.py
@@ -1,4 +1,10 @@
+import flask
 
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
-if __name__ == "__main__":
-    print("Hello")
+@app.route('/', methods=['GET'])
+def helloWorld():
+    return "Hello World"
+
+app.run()
