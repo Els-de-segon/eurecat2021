9cf9adb7539eb91eb92663c64ccf0c15c658593e
Marc <marc.martin1999@gmail.com>
2021-11-27 15:48:33 +0100

    data management

--- ia/scripts/csv-manager.py
+++ ia/scripts/csv-manager.py
@@ -1,6 +1,12 @@
 import os
 import pandas as pd
 
-os.chdir("/dataset/amoni.csv")
-amoni =pd.read_csv("/dataset/amoni.csv")
-print(amoni)
+
+amoni =pd.read_csv(os.path.dirname(__file__) + "/dataset/amoni.csv")
+water = pd.read_csv(os.path.dirname(__file__) + "/dataset/aigua.csv")
+air = pd.read_csv(os.path.dirname(__file__) + "/dataset/aire.csv")
+
+result = amoni.set_index('row_date').join(water.set_index('row_date'),how = "inner",lsuffix= "_amoni",rsuffix="_water")
+result = result.join(air.set_index('row_date'),how = "inner")
+result = result[["value_amoni","value_water","value","is_drift","dangerous_drift"]]
+result.to_csv(os.path.dirname(__file__) + "/dataset/data.csv")
